a3d7f47bc0927d9c311b8dc65c0f2e7e893d82ad
Sergio Pellegrini <sergio@plusplusminus.co.za>
2015-05-29 18:25:37 +0200

    0.0.19

--- wp-content/themes/aevitas/templates/archive.php
+++ wp-content/themes/aevitas/templates/archive.php
@@ -5,16 +5,16 @@
 
 			<?php while ( have_posts() ) : the_post(); ?>
 				<div class="page_content">
-					<?php if ( $brew_options['breadcrumb'] == 0) { ?>
+
 
 						<?php if (is_category()) { ?>
 							<h1 class="page_content--title">
-								<span><?php _e( 'Posts Categorized:', 'bonestheme' ); ?></span> <?php single_cat_title(); ?>
+								<span><?php _e( 'Posts Categorized:', 'aevitas' ); ?></span> <?php single_cat_title(); ?>
 							</h1>
 
 						<?php } elseif (is_tag()) { ?>
 							<h1 class="page_content--title">
-								<span><?php _e( 'Posts Tagged:', 'bonestheme' ); ?></span> <?php single_tag_title(); ?>
+								<span><?php _e( 'Posts Tagged:', 'aevitas' ); ?></span> <?php single_tag_title(); ?>
 							</h1>
 
 						<?php } elseif (is_author()) {
@@ -23,36 +23,38 @@
 						?>
 							<h1 class="page_content--title">
 
-								<span><?php _e( 'Posts By:', 'bonestheme' ); ?></span> <?php the_author_meta('display_name', $author_id); ?>
+								<span><?php _e( 'Posts By:', 'aevitas' ); ?></span> <?php the_author_meta('display_name', $author_id); ?>
 
 							</h1>
 						<?php } elseif (is_day()) { ?>
 							<h1 class="page_content--title">
-								<span><?php _e( 'Daily Archives:', 'bonestheme' ); ?></span> <?php the_time('l, F j, Y'); ?>
+								<span><?php _e( 'Daily Archives:', 'aevitas' ); ?></span> <?php the_time('l, F j, Y'); ?>
 							</h1>
 
 						<?php } elseif (is_month()) { ?>
 								<h1 class="page_content--title">
-									<span><?php _e( 'Monthly Archives:', 'bonestheme' ); ?></span> <?php the_time('F Y'); ?>
+									<span><?php _e( 'Monthly Archives:', 'aevitas' ); ?></span> <?php the_time('F Y'); ?>
 								</h1>
 
 						<?php } elseif (is_year()) { ?>
 								<h1 class="page_content--title">
-									<span><?php _e( 'Yearly Archives:', 'bonestheme' ); ?></span> <?php the_time('Y'); ?>
+									<span><?php _e( 'Yearly Archives:', 'aevitas' ); ?></span> <?php the_time('Y'); ?>
 								</h1>
 
 						<?php } elseif (is_post_type_archive()) { ?>
 								<h1 class="page_content--title">
 									<?php post_type_archive_title(); ?>
 								</h1>
+						<?php } elseif( is_tax() ) {
+						    global $wp_query;
+						    $term = $wp_query->get_queried_object();
+						    $title = $term->name;
+						    ?>
+							    <h1 class="page_content--title">
+									<span><?php _e( $title, 'aevitas' ); ?></span>
+								</h1>
+
 						<?php } ?>
-
-					<?php } else { ?>
-
-						<?php get_template_part( 'breadcrumb' ); ?>
-						
-					<?php } ?>
-			
 				</div>
 			<?php endwhile; ?>
 		<?php endif; ?>
