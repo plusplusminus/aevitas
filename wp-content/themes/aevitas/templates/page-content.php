51a23217362a209deba97eab14a550f7826f8ccb
Sergio Pellegrini <sergio@plusplusminus.co.za>
2015-06-01 17:33:33 +0200

    0.0.19

---
+++ wp-content/themes/aevitas/templates/page-content.php
@@ -0,0 +1,17 @@
+<article id="post-<?php the_ID(); ?>" <?php post_class('article_post clearfix'); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting"> 
+	<div class="container">
+		<?php if ( have_posts() ) : ?>
+
+			<?php while ( have_posts() ) : the_post(); ?>
+				<div class="post_content">
+					<div class="post_entry">
+						<?php the_content(); ?>
+					</div>
+					<hr class="section_break"/>
+				</div>
+			<?php endwhile; ?>
+		<?php endif; ?>
+		<?php wp_reset_query(); ?>
+		<hr class="section_break"/>
+	</div>			
+</article><?php // end #wrapper ?>
