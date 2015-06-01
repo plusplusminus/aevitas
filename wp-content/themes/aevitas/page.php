8539c659f59f2fd65f6e0867c1a1654dded51649
Sergio Pellegrini <sergio@plusplusminus.co.za>
2015-06-01 17:34:26 +0200

    0.0.19

--- wp-content/themes/aevitas/page.php
+++ wp-content/themes/aevitas/page.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 
-	<?php get_template_part('templates/page/page','content'); ?>
+	<?php get_template_part('templates/page','content'); ?>
 
 	<?php get_template_part('templates/section','info'); ?>
 
