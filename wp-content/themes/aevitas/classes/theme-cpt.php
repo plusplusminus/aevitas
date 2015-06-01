6fd11e21296dd94531ad4c3f67b9c794e2e52328
Sergio Pellegrini <sergio@plusplusminus.co.za>
2015-06-01 15:50:45 +0200

    0.0.19

--- wp-content/themes/aevitas/classes/theme-cpt.php
+++ wp-content/themes/aevitas/classes/theme-cpt.php
@@ -552,17 +552,17 @@ class tpbCustomPostTypes {
 
 	}
 
-	public function search_posts() {
+	public function search_posts($facets) {
 		include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
 		if ( is_plugin_active( 'swiftype-search/swiftype.php' ) ) {
 			try {
-				return $this->swiftype_post_search();
+				return $this->swiftype_post_search($facets);
 			} catch ( Exception $e ) {}
 		}
 		return $this->wordpress_search( $params );
 	}
 
-	public function swiftype_post_search() {
+	public function swiftype_post_search($facets) {
 		global $post;
 
 		$api_key     = get_option( 'swiftype_api_key' );
@@ -572,39 +572,19 @@ class tpbCustomPostTypes {
 
 		$stack = array();
 
+		foreach ($facets as $facet) {
+			$params['filters[posts]['.$facet['tax'].']'][] = $facet['id'];
+		}
 
-		$taxonomies[0] = array('name'=>'Type','slug'=>'type' );
-	    $taxonomies[1] = array('name'=>'Location','slug'=>'location' );
-	    $taxonomies[2] = array('name'=>'Venue','slug'=>'venue' );
-	    $taxonomies[3] = array('name'=>'Setting','slug'=>'setting' );
-	    $taxonomies[4] = array('name'=>'Style','slug'=>'style' );
-	    $taxonomies[5] = array('name'=>'Culture/Religion','slug'=>'culture' );
-	    $taxonomies[6] = array('name'=>'Category','slug'=>'category' );
-
-	    foreach ($taxonomies as $taxonomy) {
-	    	$terms = wp_get_post_terms($post->ID, $taxonomy['slug'], array("fields" => "ids"));
-	    	$stack = array_merge($stack,array_values($terms));
-	    }
-
-
-	    $params['filters[posts][terms]'] = $stack;
+		$params['facets[posts]'] = array('type','location','venue','setting','style','culture');
 
 		$params['per_page'] = 10;
 		$params['page'] = 1;
-		$params['fetch_fields[posts]'] = array("external_id");
+		
 
 		$swiftype_result = $client->search($engine_slug, 'posts','', $params);
 
-		$related_posts = array();
-
-		foreach ( $swiftype_result['records']['posts'] as $rel ) {
-			
-			$id = $rel['external_id'];
-			if ( $post && $post->ID == $id ) continue;
-			$related_posts[] = $id;
-		}
-
-		return $related_posts;
+		return $swiftype_result;
 	
 	}
 	
---
+++ wp-content/themes/aevitas/templates/template-search.php
@@ -0,0 +1,55 @@
+<?php /* Template Name : Search */ ?>
+<?php get_header(); ?>
+
+<?php
+	global $cpt; 
+	$facets = isset($_REQUEST['formdata']) : $_REQUEST['formdata'] ? array();
+
+	$results = $cpt->search_posts();
+?>
+
+
+
+<?php
+// Exclude categories on the homepage.
+
+$query_args = array(
+  'post_type' => array('any'), 
+  'post__in'=>$results['result'],
+  'orderby' => 'post__in',
+  'posts_per_page'=>-1
+);
+
+query_posts( $query_args );
+
+?>
+
+<section class="section_blog">  
+	<div class="container">
+		<div class="section_blog--heading">
+			<h2 class="section_blog--title">Filter Results</h2>
+		</div>
+		<?php if ( have_posts() ) : ?>
+			<div class="blog_row">
+				<?php while ( have_posts() ) : the_post(); ?>
+				  	<article id="post-<?php the_ID(); ?>" <?php post_class('blog_article css-hover-vertical clearfix'); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
+				    	
+				    	<figure class="blog_image">
+				    		<?php the_post_thumbnail('grid',array('class'=>'img-responsive')); ?>
+				    		<figcaption class="blog_content">
+
+			    				<h3 class="content_inner--title"><span><?php the_title(); ?></span></h3>
+
+							</figcaption>
+							<a class="blog_article--link" href="<?php the_permalink();?>">&nbsp;</a>
+						</figure>
+					</article>
+				<?php endwhile; ?>
+			</div>
+		<?php endif; ?>
+		<?php wp_reset_query(); ?>
+		<hr class="section_break"/>
+	</div>	
+</section> <?php // end #wrapper ?>
+
+<?php get_footer(); ?>
