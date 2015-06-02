9e67ba4f4acfacfa321370f9d11fa0efe88edf78
14850842 <sergio@plusplusminus.co.za>
2015-06-02 15:09:07 +0200

    0.0.1

--- wp-config.php
+++ wp-config.php
@@ -16,17 +16,17 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'aevitas');
+define('DB_NAME', 'aevitasvwyf_db53');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'clienfvwyf_53');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', 'HepSU6i8');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
-//define('DB_HOST', 'dedi95.flk1.host-h.net');
+// define('DB_HOST', 'localhost');
+define('DB_HOST', 'dedi95.flk1.host-h.net');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
