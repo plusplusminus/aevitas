4b8d90d7f1251d3e926f37d94559578e5ae0ff28
Sergio Pellegrini <sergio@plusplusminus.co.za>
2015-06-03 08:58:26 +0200

    1.0.1

--- wp-content/themes/aevitas/library/js/ppm.js
+++ wp-content/themes/aevitas/library/js/ppm.js
@@ -15,11 +15,6 @@ jQuery(document).ready(function(){
               content : '<div class="fancybox-image" style="background-image:url(' + this.href + '); background-size: contain; background-position:50% 50%;background-repeat:no-repeat;height:100%;width:100%;" /></div>'
           });
       },
-      tpl : {
-        closeBtn : '<button type="button" class="fancy-box-close"><svg class="svg shape-close-cross"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#shape-close-cross"></use></svg></button>',
-        next     : '<div id="slider-next"><a title="Next" class="fancybox-nav fancybox-next" href="javascript:;"></a></div>',
-        prev     : '<div id="slider-prev"><a title="Previous" class="fancybox-nav fancybox-prev" href="javascript:;"></a></div>'
-      }
     });
 
   var ias = jQuery.ias({
