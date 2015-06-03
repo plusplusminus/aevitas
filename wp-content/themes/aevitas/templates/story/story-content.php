38409eab22a32956322f0ce8187e00beee78f8f9
Sergio Pellegrini <sergio@plusplusminus.co.za>
2015-06-03 08:36:51 +0200

    1.0.1

--- wp-content/themes/aevitas/templates/story/story-content.php
+++ wp-content/themes/aevitas/templates/story/story-content.php
@@ -1,22 +1,24 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class('article_post clearfix'); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting"> 
 	<div class="container">
-		<?php if ( have_posts() ) : ?>
-			<div class="post_content">
-				<?php while ( have_posts() ) : the_post(); ?>
-					
-						<div class="post_entry">
-							<?php the_content(); ?>
-						</div>
+		<div class="post_content">
+			<?php if ( have_posts() ) : ?>
+				
+					<?php while ( have_posts() ) : the_post(); ?>
+						
+							<div class="post_entry">
+								<?php the_content(); ?>
+							</div>
 
-				<?php endwhile; ?>
-				<hr class="section_break"/>
-				<?php get_template_part('templates/story/story','related'); ?>
+					<?php endwhile; ?>
 
-				<?php get_template_part('templates/post/post','social'); ?>
-			</div>
-		<?php endif; ?>
+			<?php endif; ?>
 
-		<?php wp_reset_query(); ?>
+			<?php wp_reset_query(); ?>
+			<hr class="section_break"/>
+			<?php get_template_part('templates/story/story','related'); ?>
+
+			<?php get_template_part('templates/post/post','social'); ?>
+		</div>
 		
 	</div>			
 </article><?php // end #wrapper ?>
