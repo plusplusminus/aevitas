de2c1b810e241795b611e56d9c10ff33a444ff23
Sergio Pellegrini <sergio@plusplusminus.co.za>
2015-06-03 08:35:34 +0200

    1.0.1

--- wp-content/themes/aevitas/templates/story/story-content.php
+++ wp-content/themes/aevitas/templates/story/story-content.php
@@ -1,19 +1,21 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class('article_post clearfix'); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting"> 
 	<div class="container">
 		<?php if ( have_posts() ) : ?>
+			<div class="post_content">
+				<?php while ( have_posts() ) : the_post(); ?>
+					
+						<div class="post_entry">
+							<?php the_content(); ?>
+						</div>
 
-			<?php while ( have_posts() ) : the_post(); ?>
-				<div class="post_content">
-					<div class="post_entry">
-						<?php the_content(); ?>
-					</div>
-					<hr class="section_break"/>
-					<?php get_template_part('templates/story/story','related'); ?>
+				<?php endwhile; ?>
+				<hr class="section_break"/>
+				<?php get_template_part('templates/story/story','related'); ?>
 
-					<?php get_template_part('templates/post/post','social'); ?>
-				</div>
-			<?php endwhile; ?>
+				<?php get_template_part('templates/post/post','social'); ?>
+			</div>
 		<?php endif; ?>
+
 		<?php wp_reset_query(); ?>
 		
 	</div>			
