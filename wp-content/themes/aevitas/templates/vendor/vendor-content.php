88c1ed5b6df0b8a5a7adca0658d467d69b5c1650
Sergio Pellegrini <sergio@plusplusminus.co.za>
2015-06-08 08:40:59 +0200

    Wedding

---
+++ wp-content/themes/aevitas/templates/vendor/vendor-content.php
@@ -0,0 +1,24 @@
+<article id="post-<?php the_ID(); ?>" <?php post_class('article_post clearfix'); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting"> 
+	<div class="container">
+		<div class="post_content">
+			<?php if ( have_posts() ) : ?>
+				
+					<?php while ( have_posts() ) : the_post(); ?>
+						
+							<div class="post_entry">
+								<?php the_content(); ?>
+							</div>
+
+					<?php endwhile; ?>
+
+			<?php endif; ?>
+
+			<?php wp_reset_query(); ?>
+			<hr class="section_break"/>
+			<?php get_template_part('templates/story/story','related'); ?>
+
+			<?php get_template_part('templates/post/post','social'); ?>
+		</div>
+		
+	</div>			
+</article><?php // end #wrapper ?>
