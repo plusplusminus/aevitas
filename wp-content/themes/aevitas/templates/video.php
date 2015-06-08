84f392423cdaeac33ea3a35810e2d8f3b6fa5b68
Sergio Pellegrini <sergio@plusplusminus.co.za>
2015-06-08 09:47:19 +0200

    Wedding

---
+++ wp-content/themes/aevitas/templates/video.php
@@ -0,0 +1,11 @@
+<?php global $post; ?>
+
+<?php $url = get_post_meta($post->ID,'_ppm_video_embed',true); ?>
+<?php echo $url; ?>
+<?php if (!empty($url)) : ?>
+	<aside class="post_video">
+		<?php echo wp_oembed_get( $url ); ?>
+	</aside>
+<?php endif; ?>
+
+
