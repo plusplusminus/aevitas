b3c60e93d3a57a5b29b06c9c14839a2535e1a522
Sergio Pellegrini <sergio@plusplusminus.co.za>
2015-06-15 11:19:48 +0200

    0.1.0

--- wp-content/themes/aevitas/library/js/ppm.js
+++ wp-content/themes/aevitas/library/js/ppm.js
@@ -4,6 +4,10 @@ jQuery(document).ready(function(){
 
   jQuery('.selectize').selectize({
       sortField: 'text',
+      render:       {
+        item: function(data) {
+          return "<div data-value='"+data.value+"' data-tax='"+data.taxonomy+"' class='item'>"+data.label+" </div>";
+      },
       onChange: function(value) {
         console.log(this);
 
