823b2da150b1e8e4735a46a0eeefe323b783846a
Sergio Pellegrini <sergio@plusplusminus.co.za>
2015-06-15 11:20:37 +0200

    0.1.0

--- wp-content/themes/aevitas/library/js/ppm.js
+++ wp-content/themes/aevitas/library/js/ppm.js
@@ -7,6 +7,7 @@ jQuery(document).ready(function(){
       render:       {
         item: function(data) {
           return "<div data-value='"+data.value+"' data-tax='"+data.taxonomy+"' class='item'>"+data.label+" </div>";
+        },
       },
       onChange: function(value) {
         console.log(this);
