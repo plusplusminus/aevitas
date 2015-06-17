f38db77f82bddeb5b0afc9cd3e7019c813f128b5
Sergio Pellegrini <sergio@plusplusminus.co.za>
2015-06-17 14:30:21 +0200

    123

--- wp-content/themes/aevitas/templates/section-instagram.php
+++ wp-content/themes/aevitas/templates/section-instagram.php
@@ -1,9 +1,13 @@
 <?php global $tpb_options; ?>
 
-<?php dynamic_sidebar( 'sidebar-1' ); ?>
+<div id="%1$s" class="section_instagram">
+	<div class="container">
+		<?php dynamic_sidebar( 'sidebar-1' ); ?>
 
-<div class="instagram_follow">
-	<a href="<?php echo $tpb_options['instagram_url'];?>" class="btn btn-standard"><span class="fa fa-instagram"></span> Follow Us on Instagram</a>
+		<div class="instagram_follow">
+			<a href="<?php echo $tpb_options['instagram_url'];?>" class="btn btn-standard"><span class="fa fa-instagram"></span> Follow Us on Instagram</a>
+		</div>
+
+		<hr class="section_break"/>
+	</div>
 </div>
-
-<hr class="section_break"/>
