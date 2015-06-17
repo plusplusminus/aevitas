5d5c17079608cebfd68acd43377fef21b37db488
Sergio Pellegrini <sergio@plusplusminus.co.za>
2015-06-17 13:33:05 +0200

    123

--- wp-content/themes/aevitas/functions.php
+++ wp-content/themes/aevitas/functions.php
@@ -41,6 +41,18 @@ function ppm_scripts_and_styles() {
     }
 }
 
+add_action( 'widgets_init', 'theme_slug_widgets_init' );
+function theme_slug_widgets_init() {
+    register_sidebar( array(
+        'name' => __( 'Main Sidebar', 'theme-slug' ),
+        'id' => 'sidebar-1',
+        'description' => __( 'Widgets in this area will be shown on all posts and pages.', 'theme-slug' ),
+        'before_widget' => '<li id="%1$s" class="widget %2$s">',
+    'after_widget'  => '</li>',
+    'before_title'  => '<h2 class="widgettitle">',
+    'after_title'   => '</h2>',
+    ) );
+}
 
 add_filter('redux/options/tpb_options/sections', 'child_sections');
 function child_sections($sections){
