dc9824e99182d585d5a401a37bd6553980990178
Sergio Pellegrini <sergio@plusplusminus.co.za>
2015-06-17 15:04:00 +0200

    123

--- wp-content/themes/aevitas/templates/blog/search.php
+++ wp-content/themes/aevitas/templates/blog/search.php
@@ -22,29 +22,19 @@ global $post;
 		<div class="work_filter">
 			<ul class="filter_list">
 				<?php foreach ($taxonomies as $taxonomy) {
-					if ( ! empty( $taxonomy ) && ! is_wp_error( $taxonomy ) ) {
-						echo '<li>';
-							echo '<div class="btn-group">';
-								echo '<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-									    '.$taxonomy['name'].' <span class="caret"></span>
-									  </button>';
-						        echo '<ul class="dropdown-menu" role="menu">';
-								foreach ( $taxonomy['terms'] as $term ) {
-								 	$term_link = get_term_link( $term );
-		   
-									    // If there was an error, continue to the next term.
-									    if ( is_wp_error( $term_link ) ) {
-									        continue;
-									    }
+						if ( ! empty( $taxonomy ) && ! is_wp_error( $taxonomy ) ) {
 
-									    // We successfully got a link. Print it out.
-									    echo '<li><a href="' . esc_url( $term_link ) . '">' . $term->name . '</a></li>';
-								}
-								echo '</ul>';
-							echo '</div>';
-						echo '</li>';
+				
+								echo '<li>';
+									get_tax_opts($taxonomy['slug']);
+								echo '</li>';
+								echo '<button type="submit" class="submit_button">Filter</button>';
+								echo '<li>'
+
+							
+						}
 					}
-				}
+					?>
 				?>
 			</ul>
 			<div class="clearfix"></div>
