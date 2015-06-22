e632beac3846d77e03d5312b089f0545a6c738dd
Sergio Pellegrini <sergio@plusplusminus.co.za>
2015-06-22 12:43:36 +0200

    0.0.2

--- wp-content/themes/aevitas/library/js/ppm.js
+++ wp-content/themes/aevitas/library/js/ppm.js
@@ -453,7 +453,7 @@ var Selectizer = function () {
           if (response.data.location) {
             $location[0].selectize.enable();
             var loc = $location[0].selectize.getValue();
-            //$location[0].selectize.clearOptions();
+            $location[0].selectize.clearOptions();
             $location[0].selectize.addOption(response.data.location);
             $location[0].selectize.setValue(loc,true);
             $location[0].selectize.refreshOptions(false);
@@ -529,9 +529,9 @@ var Selectizer = function () {
     clearOption: function(e) {
 
       var selectize = this;
-      var sel = selectize.getValue();
+      var loc = selectize.getValue();
 
-      console.log(sel);
+      console.log(loc);
       _.remove(facets, function(n) {
         console.log(n.tax);
         return n.tax == selectize.settings.tax;
@@ -550,73 +550,17 @@ var Selectizer = function () {
           var html = '';
 
           if (response.data.location) {
-            $location[0].selectize.enable();
-            var loc = $location[0].selectize.getValue();
+            $location[0].selectize.clear();
             $location[0].selectize.clearOptions();
-            $location[0].selectize.addOption(response.data.location);
-            $location[0].selectize.setValue(loc,true);
-            $location[0].selectize.refreshOptions(false);
+            $location[0].selectize.load(function(callback) {
+                callback(response.data.location);
+            });
+            $location[0].selectize.enable();
+            
           } else {
             $location[0].selectize.disable();
           }
 
-          if (response.data.style) {         
-            $style[0].selectize.enable();
-            var style = $style[0].selectize.getValue();
-            $style[0].selectize.clearOptions();
-            $style[0].selectize.addOption(response.data.style);
-            $style[0].selectize.setValue(style,true);
-            $style[0].selectize.refreshOptions(false);
-          } else {
-            $style[0].selectize.disable();
-          }
-
-          if (response.data.venue) {
-            $venue[0].selectize.enable();
-            var venue = $venue[0].selectize.getValue();
-            $venue[0].selectize.clearOptions();
-            $venue[0].selectize.addOption(response.data.venue);
-            $venue[0].selectize.setValue(venue,true);
-            $venue[0].selectize.refreshOptions(false);
-          } else {
-            $venue[0].selectize.disable();
-          }
-
-          if (response.data.type) {
-            $type[0].selectize.enable();
-            var type = $type[0].selectize.getValue();
-            $type[0].selectize.clearOptions();
-            $type[0].selectize.addOption(response.data.type);
-            $type[0].selectize.setValue(type,true);
-            $type[0].selectize.refreshOptions(false);
-          } else {
-            $type[0].selectize.disable();
-          }
-
-          if (response.data.culture) {
-
-            $culture[0].selectize.enable();
-            var culture = $culture[0].selectize.getValue();
-            $culture[0].selectize.clearOptions();
-            $culture[0].selectize.addOption(response.data.culture);
-            $culture[0].selectize.setValue(culture,true);
-            $culture[0].selectize.refreshOptions(false);
-          } else {
-            $culture[0].selectize.disable();
-          }
-
-          if (response.data.setting) {
-            $setting[0].selectize.enable();
-            var setting = $setting[0].selectize.getValue();
-            $setting[0].selectize.clearOptions();
-            $setting[0].selectize.addOption(response.data.setting);
-            $setting[0].selectize.setValue(setting,true);
-            $setting[0].selectize.refreshOptions(false);
-          } else {
-            $setting[0].selectize.disable();
-          }
-
-          
           jQuery('.submit_button').removeAttr("disabled").text('Filter');
 
         }
