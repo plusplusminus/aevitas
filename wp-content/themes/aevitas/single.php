0608dbc16d0a0e2896e1c4f483ea507fb52ec5de
14850842 <sergio@plusplusminus.co.za>
2015-06-23 11:17:46 +0200

    123

--- wp-content/themes/aevitas/single.php
+++ wp-content/themes/aevitas/single.php
@@ -4,6 +4,6 @@
 
   	<?php get_template_part('templates/post/post','content'); ?>
 
-    <?php get_template_part('templates/section','work'); ?>
+    <?php //get_template_part('templates/section','work'); ?>
 
 <?php get_footer(); ?>
